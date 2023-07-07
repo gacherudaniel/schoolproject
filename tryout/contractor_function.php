513c65030baa69ab4a76ea09f4b585987c1b10b4
gacherudaniel <danielgacheru01@gmail.com>
2023-07-07 06:57:57 +0300

    Added a login page
    Added some backend code to the login page

--- tryout/contractor_function.php
+++ tryout/contractor_function.php
@@ -8,29 +8,45 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $password = $_POST['password'];
     $status = $_POST['status'];
 
-    // Prepare SQL statement
-    $sql = "INSERT INTO contractors (name, email, password, status) 
+    // Prepare SQL statements
+    $contractorSql = "INSERT INTO contractors (name, email, password, status) 
             VALUES (:name, :email, :password, :status)";
+    $userSql = "INSERT INTO users (name, email, password, role) 
+            VALUES (:name, :email, :password, 'contractor')";
 
-    $query = $dbh->prepare($sql);
+    // Begin transaction
+    $dbh->beginTransaction();
 
-    // Bind the parameters
-    $query->bindParam(':name', $name);
-    $query->bindParam(':email', $email);
-    $query->bindParam(':password', $password);
-    $query->bindParam(':status', $status);
+    try {
+        // Insert into contractors table
+        $contractorQuery = $dbh->prepare($contractorSql);
+        $contractorQuery->bindParam(':name', $name);
+        $contractorQuery->bindParam(':email', $email);
+        $contractorQuery->bindParam(':password', $password);
+        $contractorQuery->bindParam(':status', $status);
+        $contractorQuery->execute();
 
-    // Execute SQL statement
-    if ($query->execute()) {
+        // Insert into users table
+        $userQuery = $dbh->prepare($userSql);
+        $userQuery->bindParam(':name', $name);
+        $userQuery->bindParam(':email', $email);
+        $userQuery->bindParam(':password', $password);
+        $userQuery->execute();
+
+        // Commit the transaction
+        $dbh->commit();
+
         echo "<script>alert('New record created successfully');</script>";
-            header("Location: contractor_management.php");
-            exit;
-
-    } else {
-        echo "Error: " . $sql . "<br>" . $query->errorInfo();
+        header("Location: contractor_management.php");
+        exit;
+    } catch (PDOException $e) {
+        // Rollback the transaction in case of error
+        $dbh->rollBack();
+        echo "Error: " . $e->getMessage();
     }
 }
 
 // Close the database connection
 $dbh = null;
+
 ?>
